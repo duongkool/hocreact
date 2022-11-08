8d4b376d672432ec140f30c2cbca81046d6c7891
duongkool <duongkool98@gmail.com>
2022-11-08 10:05:17 +0700

    video29

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,20 +1,35 @@
 import React from "react";
 class DisplayInfor extends React.Component {
+    state = {
+        isShowListUser: true
+    }
+    handleShowHide = () => {
+        this.setState({
+            isShowListUser: !this.state.isShowListUser
+        })
+    }
     render() {
         const { listUsers } = this.props;
-        console.log(listUsers)
         return (
             <div>
-                {listUsers.map((user) => {
-                    return (
-                        <div key={user.id}>
-                            <div>My name is {user.name}</div>
-                            <div>My age's {user.age}</div>
-                            <hr />
-                        </div>
-                    )
-                })}
-
+                <div>
+                    <button onClick={() => { this.handleShowHide() }}>
+                        {this.state.isShowListUser === true ? "Hide list user" : "Show list user"}
+                    </button>
+                </div>
+                {this.state.isShowListUser &&
+                    <div>
+                        {listUsers.map((user) => {
+                            return (
+                                <div className={+user.age > 18 ? "blue" : "red"} key={user.id}>
+                                    <div>My name is {user.name}</div>
+                                    <div>My age's {user.age}</div>
+                                    <hr />
+                                </div>
+                            )
+                        })}
+                    </div>
+                }
             </div>
         )
     }
